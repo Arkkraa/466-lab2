2a1fc033bb7211b2544779484fb73fa678a77280
Arkkra <gen.rkd@hotmail.com>
2015-10-04 15:40:13 -0700

    create term frequency model

--- model_data.py
+++ model_data.py
@@ -5,11 +5,19 @@ def model_data():
 	with open('parsed_text.json', 'r+') as fp:
 		data = {int(key): value for key, value in json.load(fp).items()}
 
-
-	term_freq = dict()
+	model = dict()
 	for key, value in data.items():
-		
-	#pprint.pprint(data)
+		term_freq = dict()
+		for word in value['text']:
+			if word not in term_freq.keys():
+				term_freq[word] = 1
+			else:
+				term_freq[word] += 1
+		model[key] = term_freq
+
+	
+	
+	#pprint.pprint(model)
 
 if __name__ == '__main__':
 	model_data()
