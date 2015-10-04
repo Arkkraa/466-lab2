8757aa8947e237020b3752957ca2490f921ee101
Arkkra <gen.rkd@hotmail.com>
2015-10-04 12:33:36 -0700

    remove meta_data stuff and make word file thing

--- parser.py
+++ parser.py
@@ -1,21 +1,25 @@
 import json
 import pprint
+import string
 
-def create_meta_data(results):
-	meta_data_keys = ['first', 'last', 'PersonType', 'date', 'house', 'Committee']
-	meta_data_file = dict()
+def create_words_data(results):
+	words_file = dict()
+	i = 0
+
 	for r in results:
-		meta_data = dict()
-		meta_data = {key:value for key, value in r.items() if key in meta_data_keys}
-		meta_data_file[r['pid']] = meta_data
+		text = r['text'].split()
+		text = [word.strip(string.punctuation) for word in text]
+		words = {'pid': r['pid'], 'text': text}
+		if words not in words_file.values():
+			words_file[i] = words
+		i += 1
 
-	f = open("meta_data.json", 'w+')
-	json.dump(meta_data_file, f)
-
+	#pprint.pprint(words_file)
+		
 def main():
 	fp = open("SB277Utter.json", 'r+')
 	results = json.loads(fp.read())
-	create_meta_data(results)
+	create_words_data(results)
 
 if __name__ == '__main__':
 	main()
---
+++ read_data.py
@@ -0,0 +1,5 @@
+import json
+import pprint
+
+if __name__ == '__main__':
+	read_meta_data()
