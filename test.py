a2731e6de1288417dd009e9f5ed9a9162b783769
jlguevara <jlguevara81@gmail.com>
2015-10-06 01:06:46 -0700

    cleaned up code, still needs testing

--- test.py
+++ test.py
@@ -18,62 +18,82 @@ def stripWord(word):
    """Generates a term based on the word given"""
    return word.strip(' ' + string.punctuation)
 
-   
+def getData(filename):
+   """ Return metadata and documents from filename """
 
-f = open('input.json')
-records = json.loads(f.read())
+   f = open(filename)
+   records = json.loads(f.read())
 
-metadata = []
-documents = []
-vocabulary = {}
+   metadata = []
+   documents = []
 
-stopwords =  getStopwords()
-porter = stemmer.PorterStemmer()
+   stopwords =  getStopwords()
+   porter = stemmer.PorterStemmer()
 
-for r in records:
-   # extract people data 
-   meta= {}
-   meta['pid'] = r['pid']
-   meta['first'] = r['first']
-   meta['last'] = r['last']
-   meta['PersonType'] = r['PersonType']
-   meta['date'] = r['date']
-   meta['house'] = r['house']
-   meta['Committee'] = r['Committee']
-   metadata.append(meta)
+   for r in records:
+      # extract people data 
+      meta= {}
+      meta['pid'] = r['pid']
+      meta['first'] = r['first']
+      meta['last'] = r['last']
+      meta['PersonType'] = r['PersonType']
+      meta['date'] = r['date']
+      meta['house'] = r['house']
+      meta['Committee'] = r['Committee']
+      metadata.append(meta)
 
-   termFrequency = {}
+      termFrequency = {}
 
-   for word in r['text'].split():
-      word = stripWord(word)
-      word = word.lower()
+      for word in r['text'].split():
+         word = stripWord(word)
+         word = word.lower()
 
-      # in case a word is just made up of punctuation like !!
-      if not word:
-         continue
+         # in case a word is just made up of punctuation like !!
+         if not word:
+            continue
+         
+         if word not in stopwords:
+            word = porter.stem(word, 0, len(word) - 1)
+            termFrequency[word] = termFrequency.get(word, 0) + 1
       
-      if word not in stopwords:
-         word = porter.stem(word, 0, len(word) - 1)
-         termFrequency[word] = termFrequency.get(word, 0) + 1
-   
-   documents.append(termFrequency)
+      documents.append(termFrequency)
 
-   for term in termFrequency:
-      # update document frequencyi for each term
-      vocabulary[term] = vocabulary.get(term,0) + 1
+      return metadata, documents
 
-# compute term frequency * inverse document frequency
-numOfDocuments = len(documents)
-for d in documents:
 
-   # find the max frequency in the document
-   maxFrequency = 0
-   for term in d:
-      freq = d[term]
-      if freq > maxFrequency:
-         maxFrequency = freq
+def getVocab(documents):
+   """ Generate the vocabulary from the given documents """
 
-   for term in d:
-      tf = d[term] / maxFrequency
-      idf = log(numOfDocuments / vocabulary[term],2)
-      d[term] = tf * idf
+   vocabulary = {}
+
+   for termFrequencies in documents:
+
+      for term in termFrequencies:
+         # update document frequencyi for each term
+         vocabulary[term] = vocabulary.get(term,0) + 1
+   return vocabulary
+
+
+def generateTfAndIdf(documents, vocabulary):
+   """ Compute term frequency * inverse document frequency """
+
+   numOfDocuments = len(documents)
+   for termFrequencies in documents:
+      # find the max frequency in the document
+      maxFrequency = 0
+      for term in termFrequencies:
+         freq = termFrequencies[term]
+         if freq > maxFrequency:
+            maxFrequency = freq
+
+      for term in termFrequencies:
+         tf = termFrequencies[term] / maxFrequency
+         idf = log(numOfDocuments / vocabulary[term],2)
+         termFrequencies[term] = tf * idf
+
+
+if __name__ == '__main__':   
+   rawdata = 'input.json' 
+   metadata, documents = getData(rawdata)
+   vocabulary = getVocab(documents)
+   generateTfAndIdf(documents, vocabulary)
