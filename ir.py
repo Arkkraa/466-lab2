ce09ef88c3850c7ecd90bf670d7f1893ce77d911
jlguevara <jlguevara81@gmail.com>
2015-10-07 20:20:30 -0700

    simple ir is working

---
+++ ir.py
@@ -0,0 +1,114 @@
+import json
+import string
+import stemmer
+import math
+import vector_utils, query_utils
+
+STOPWORDS_FILE = "stopwords.txt"
+
+def getStopwords():
+   """ Loads stopwords from file and returns a dictionary """
+   stopwords = {}
+   f = open(STOPWORDS_FILE)
+   for line in f:
+      line = line.strip()
+      stopwords[line] = True
+   return stopwords
+
+
+def stripWord(word):
+   """Generates a term based on the word given"""
+   return word.strip(string.punctuation)
+
+def getData(filename):
+   """ Return metadata and documents from filename """
+
+   f = open(filename)
+   records = json.loads(f.read())
+
+   metadata = []
+   documents = []
+
+   stopwords =  getStopwords()
+   porter = stemmer.PorterStemmer()
+
+   for r in records:
+      # extract people data 
+      meta= {}
+      meta['pid'] = r['pid']
+      meta['first'] = r['first']
+      meta['last'] = r['last']
+      meta['PersonType'] = r['PersonType']
+      meta['date'] = r['date']
+      meta['house'] = r['house']
+      meta['Committee'] = r['Committee']
+      metadata.append(meta)
+
+      termFrequency = {}
+
+      for word in r['text'].split():
+         word = stripWord(word)
+         word = word.lower()
+
+         # in case a word is just made up of punctuation like !!
+         if not word:
+            continue
+         
+         if word not in stopwords:
+            word = porter.stem(word, 0, len(word) - 1)
+            termFrequency[word] = termFrequency.get(word, 0) + 1
+      
+      documents.append(termFrequency)
+
+   
+   return metadata, documents
+
+
+def getVocab(documents):
+   """ Generate the vocabulary from the given documents """
+
+   vocabulary = {}
+
+   for termFrequencies in documents:
+
+      for term in termFrequencies:
+         # update document frequencyi for each term
+         vocabulary[term] = vocabulary.get(term,0) + 1
+   return vocabulary
+
+def generateIdf(vocabulary, numOfDocuments):
+   """ Generate inverse document frequency for the given vocabulary """
+
+   for term in vocabulary:
+      inverse = float(numOfDocuments) / vocabulary[term]
+      vocabulary[term] = math.log(inverse, 2)
+
+
+def generateTf(documents, vocabulary):
+   """ Compute term frequency * inverse document frequency. Vocab assumed to be in idf """
+
+   for termFrequencies in documents:
+      # find the max frequency in the document
+      maxFrequency = 0
+      for term in termFrequencies:
+         freq = termFrequencies[term]
+         if freq > maxFrequency:
+            maxFrequency = freq
+
+      for term in termFrequencies:
+         tf = termFrequencies[term] / float(maxFrequency)
+         idf = vocabulary[term]
+         termFrequencies[term] = tf * idf
+
+
+if __name__ == '__main__':   
+   rawdata = 'input.json' 
+   metadata, documents = getData(rawdata)
+   vocabulary = getVocab(documents)
+   generateIdf(vocabulary, len(documents))
+   generateTf(documents, vocabulary)
+
+   query = raw_input('Your query: ')
+   queryVector = query_utils.queryVectorFromString(query)
+   query_utils.updateWeights(queryVector, vocabulary)
+   print query_utils.cosineSimilarity(queryVector, documents)
---
+++ query_utils.py
@@ -0,0 +1,53 @@
+# Util functions for query processing
+
+from vector_utils import cosine
+import ir
+import stemmer
+
+def updateWeights(queryVector, vocab):
+   """ Compute weights using Salton Buckley algo. Vocab is assumed to be in
+   inverse document frequency already"""
+
+   maxFreq = 0
+   for v in queryVector:
+      freq = queryVector[v]
+      if freq > maxFreq:
+         maxFreq = freq
+
+   for v in queryVector:
+      freq = queryVector[v]
+      queryVector[v] = (0.5 + (0.5 * freq) / maxFreq) * vocab[v]
+
+def cosineSimilarity(queryVector, documents):
+   """ Returns a list with the cosine similarity between the query and each
+   document in the document collection """
+
+   return map(lambda documentVector: cosine(queryVector, documentVector), documents)
+
+def queryVectorFromString(query):
+   """ Returns a query vector from input string.  Vector will contain frequency 
+   counts. Function will remove stopwords and stem remainding query terms"""
+
+   queryVector = {}
+   stopwords =  ir.getStopwords()
+   porter = stemmer.PorterStemmer()
+
+   for word in query.split():
+      word = ir.stripWord(word)
+      word = word.lower()
+
+      # in case a word is just made up of punctuation like !!
+      if not word:
+         continue
+      
+      # compute frequency of term
+      if word not in stopwords:
+         word = porter.stem(word, 0, len(word) - 1)
+         queryVector[word] = queryVector.get(word, 0) + 1
+
+   return queryVector
+
+if __name__ == '__main__':
+   query = raw_input("query: ")
+   print queryVectorFromString(query)
+   
