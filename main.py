20a2153c92700cbdbe86168a55ab70f2001e06d0
jlguevara <jlguevara81@gmail.com>
2015-10-09 14:22:17 -0700

    added README

---
+++ main.py
@@ -0,0 +1,26 @@
+
+import ir
+import query_utils
+import json
+
+if __name__ == '__main__':
+   # load saved model
+   metadata, documents, vocabulary, ogText = ir.loadSystem()
+
+
+   while True:
+      query = raw_input('Your query (Enter to quit): ')
+      if not query:
+         break
+
+      queryVector = query_utils.queryVectorFromString(query)
+      query_utils.updateWeights(queryVector, vocabulary)
+      similarities =  query_utils.cosineSimilarity(queryVector, documents)
+      results = query_utils.getTopTen(similarities)
+
+      for result in results:
+         print result[1]
+         print ogText[result[0]]
+         print
+
+      print '\n' + 80 * '*'
